d5cd8ece0e6e9b42f57f701bae51eedf88c34a53
Joao Vitor Motta Silva <vte_jsilva@uolinc.com>
2023-11-16 15:09:35 -0300

    first commit

---
+++ terraform/terraform.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0efcece6bed30fd98" //os name from aws
+  instance_type = "t2.micro"
+  key_name = "iac-noteuol"
+  tags = {
+    Name = "First Instance" //EC2 instanceName
+  }
+}
